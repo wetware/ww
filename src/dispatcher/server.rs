8e1a03077f9333302e98393e6d37d7d5de324ce8
Louis Thibault <louist87@gmail.com>
2026-08-10 15:14:11 +0300

    test(http): use a valid route preflight CID

--- src/dispatcher/server.rs
+++ src/dispatcher/server.rs
@@ -192,7 +192,9 @@ mod tests {
             _params: system_capnp::executor::CidParams,
             mut results: system_capnp::executor::CidResults,
         ) -> Promise<(), capnp::Error> {
-            results.get().set_cid("dispatch-test");
+            results
+                .get()
+                .set_cid("bafkr4if3s6yv23hd3hgfvftj2g2uwdrqazv53p36p5lqyy7n77d5t5p54a");
             Promise::ok(())
         }
     }
